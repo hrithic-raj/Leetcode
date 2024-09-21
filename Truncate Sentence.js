9685d3687da8dd5ead42d78eaf67a03c6631d71d
Hrithic <hrithicraj730@gmail.com>
2024-09-21 09:25:01 +0530

    Q35

---
+++ Truncate Sentence.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var truncateSentence = function(s, k) {
+    return s.split(' ').slice(0,k).join(' ')
+};
+
+console.log(truncateSentence("Hello how are you Contestant", 4))
+console.log(truncateSentence("What is the solution to this problem", 4))
