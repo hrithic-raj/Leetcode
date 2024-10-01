174fb2dc5c711acd0d075c2a7069b197e2aeb390
Hrithic <hrithicraj730@gmail.com>
2024-10-01 10:20:24 +0530

    Q43

--- Number of Segments in a String.js
+++ Number of Segments in a String.js
@@ -6,6 +6,6 @@ var countSegments = function(s) {
     return s.split(' ').filter(val=>val!=='').length
 };
 
-console.log(countSegments("Hello, my name is John"));//5
-console.log(countSegments("Hello"));//1
-console.log(countSegments('')); //0
+console.log(countSegments("Hello, my name is John")); //5
+console.log(countSegments("Hello")); //1
+console.log(countSegments('')); //00
