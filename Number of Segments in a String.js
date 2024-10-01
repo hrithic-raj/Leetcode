1a9757d12abaae89d7e56f64a81b6edc91f90b11
Hrithic <hrithicraj730@gmail.com>
2024-10-01 10:19:37 +0530

    Q41

---
+++ Number of Segments in a String.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSegments = function(s) {
+    return s.split(' ').filter(val=>val!=='').length
+};
+
+console.log(countSegments("Hello, my name is John"));//5
+console.log(countSegments("Hello"));//1
+console.log(countSegments('')); //0
