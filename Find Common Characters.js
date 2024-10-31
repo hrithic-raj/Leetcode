57d0ebc5e1ded20f148ce334305f60d71cb4377e
Hrithic <hrithicraj730@gmail.com>
2024-10-31 09:40:24 +0530

    Q68

---
+++ Find Common Characters.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var commonChars = function(words) {
+    
+};
+
+console.log(commonChars(["bella","label","roller"])); // Output: ["e","l","l"]
+console.log(commonChars(["cool","lock","cook"]));     // Output: ["c","o"]
