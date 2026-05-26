630ad43b59dd16141decca503646590e756f231c
Hrithic Raj <hrithicraj730@gmail.com>
2026-05-26 12:28:41 +0530

    Time: 191 ms (17.39%), Space: 67.6 MB (13.04%) - LeetHub

---
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string} word
+ * @return {number}
+ */
+var numberOfSpecialChars = function(word) {
+    const lastLower = {};
+    const firstUpper = {};
+
+    for(let i=0; i<word.length; i++){
+        let ch = word[i];
+
+        if(ch===ch.toLowerCase()){
+            lastLower[ch]=i
+        }else{
+            const lower = ch.toLowerCase();
+
+            if(firstUpper[lower]===undefined){
+                firstUpper[lower]=i;
+            }
+        }
+    }
+
+    let count = 0;
+    for (let i = 97; i <= 122; i++) {
+        const ch = String.fromCharCode(i);
+
+        if(lastLower[ch]!== undefined && 
+        firstUpper[ch] !== undefined && 
+        lastLower[ch] < firstUpper[ch]
+        ){
+            count++;
+        }
+    }
+    
+    return count;
+    
+    console.log(firstUpper)
+    console.log(lastLower)
+};
